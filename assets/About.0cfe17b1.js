96917b4a34c8d9c8405749f5ae6df4a6ca50d5a7
saulane <xsaulanex@gmail.com>
2021-06-01 07:26:50 +0200

    Updates

---
+++ assets/About.0cfe17b1.js
@@ -0,0 +1,1 @@
+import{o as a,b as o,d as s}from"./vendor.b622b277.js";const n={},r={class:"about"},t=s("h1",null,"This is an about page",-1);n.render=function(s,n){return a(),o("div",r,[t])};export default n;
